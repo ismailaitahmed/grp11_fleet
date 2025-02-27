33c21bd1037e0f50dcb31ebdf22c0662d9bdfd3a
Hale <hsteel76@gmail.com>
2025-02-26 22:20:43 -0500

    US05 - Created Feature, Step Defs  and Page.

---
+++ src/test/java/com/fleet/step_definitions/US05_Hale_StepDefs.java
@@ -0,0 +1,23 @@
+package com.fleet.step_definitions;
+
+import com.fleet.pages.US05_HalePage;
+import com.fleet.pages.US07_MainPage;
+import com.fleet.utilities.BrowserUtils;
+import com.fleet.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Random;
+
+public class US05_Hale_StepDefs {
+
+    US05_HalePage US05_Page = new US05_HalePage();
+
+
+}
