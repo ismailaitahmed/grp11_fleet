c9ca0f88fcc31c921a9cfdc58e00ed2a57bf4d98
Hale <hsteel76@gmail.com>
2025-02-26 22:20:19 -0500

    US05 - Created Feature, Step Defs  and Page.

---
+++ src/test/java/com/fleet/pages/US05_HalePage.java
@@ -0,0 +1,19 @@
+package com.fleet.pages;
+
+
+
+
+
+import com.fleet.utilities.Driver;
+
+import org.openqa.selenium.support.PageFactory;
+
+
+public class US05_HalePage {
+    public US05_HalePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+
+}
